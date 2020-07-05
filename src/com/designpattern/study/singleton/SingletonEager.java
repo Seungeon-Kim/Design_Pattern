6d0733a11a7f562d199310075dbfb8a343cead76
Seungeon-Kim <seungeon1221@gmail.com>
2020-07-05 21:58:37 +0900

    Singleton Pattern
    다양한 싱글톤 패턴 사용 방법

--- src/com/designpattern/study/singleton/SingletonEager.java
+++ src/com/designpattern/study/singleton/SingletonEager.java
@@ -1,7 +1,7 @@
 package com.designpattern.study.singleton;
 
 
-// 장점 : 멀티 쓰레드 상황에서 객체의 유일성을 보장
+// 장점 : 멀티 쓰레드 환경에서 객체의 유일성을 보장
 // 단점 : 사용하지 않더라도, 미리 할당되어 메모리 낭비
 public class SingletonEager {
     private static SingletonEager instance = new SingletonEager();
--- src/com/designpattern/study/singleton/SingletonLazyHolder.java
+++ src/com/designpattern/study/singleton/SingletonLazyHolder.java
@@ -1,5 +1,9 @@
 package com.designpattern.study.singleton;
 
+/* 장점 : JVM에게 객체의 초기화를 떠님기는 방식으로, 멀티 스레드 환경에서도 객체의 유일성 보장.
+         Syncronized 사용을 하지않아 성능 저하를 방지할 수 있다.
+         가장 추천되는 방식의 Singleton Pattern
+*/
 public class SingletonLazyHolder {
     public static SingletonLazyHolder getInstance(){
         return LazyHolder.INSTANCE;
